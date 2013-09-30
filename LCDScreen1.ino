e9c3a7688b18731b726504ccfdb6015b3d790d66
RyanEggert <Ryan.Eggert@students.olin.edu>
2013-09-29 23:31:39 -0400

    DO/MAY NOT WORK
    Use previous versions. Attempts to automatically parse custom message
    and divide into lines on LCD.

--- LCDScreen1.ino
+++ LCDScreen1.ino
@@ -12,7 +12,7 @@ LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
 
 // you can change the overall brightness by range 0 -> 255
 int brightness = 255;
-int Temp=0;
+int Temp=0; //Temp variable used for testing. Random values
 
 int blRed=3;  //Red backlight pin
 int blGrn=5;  //Green backlight pin
@@ -21,12 +21,15 @@ int blBlu=6;  //Blue backlight pin
 int TempPin=A0; //The pin into which the temp sensor output is plugged
 int Temp1=0;   //Temperature reading directly from sensor
 
+int cutoff;   //For splitting long messages into two lines for the LCD
+
 int r=1;
 int g=1;
 int b=1;
 
-String IncomingMessage;
-
+String IncomingMessage; //Custom message assembled from data coming from serial port
+String FirstLine;
+String SecondLine; //Strings used to write long messages to two lines
 void setup() 
 {
 
@@ -35,6 +38,7 @@ void setup()
 
   // Set up the LCD's number of rows and columns: 
   lcd.begin(16, 2);
+  lcd.clear();
 
   	//Set up the backlight controls
     pinMode(blRed, OUTPUT);
@@ -44,6 +48,10 @@ void setup()
     lcd.print("Temperature ");
 
     brightness = 255;
+    r=0;
+    b=10;
+    g=100;
+    setBacklight(r,g,b);
     Serial.begin(9600);
     Serial.println("1050");
   }
@@ -51,22 +59,53 @@ void setup()
 
   void loop() 
   {
-   if ( Serial.available() > 0)
-   {
+
+    Temp=random(32,100);
+    lcd.setCursor(12,0);
+    lcd.print(Temp);
+    lcd.print(char(223));
+	// if (tempmode==0)
+	// {
+	// 	lcd.print("F");
+	// }
+  lcd.setCursor(0,1);
+  Temp1=analogRead(TempPin);
+
+  lcd.print(Temp1);
+  Serial.println(Temp1);
+  if ( Serial.available() > 0)
+  {
     while(Serial.available() >0)
     {
      char recieved=Serial.read();
      if (recieved==char(003))
      {
       Serial.println("Message Received");
-      Serial.println(IncomingMessage);
-      lcd.clear();
-      lcd.print(IncomingMessage);
+      cutoff=IncomingMessage.indexOf(char(007));
+      if (cutoff!=-1) //if the incomingmessage has the bell character--this means it will be displayed on two lines
+      {
+        FirstLine=IncomingMessage[0:cutoff];
+        SecondLine=IncomingMessage[cutoff+1:length(IncomingMessage)];
+        lcd.clear();
+        lcd.print(FirstLine);
+        lcd.setCursor(0,1);
+        lcd.print(SecondLine);
+        FirstLine="";  //clear string for next message/use
+        SecondLine=""; //clear string for next message/use
+      }
+      else
+      {
+        Serial.println(IncomingMessage);
+        lcd.clear();
+        lcd.print(IncomingMessage);
+        IncomingMessage="";   
+      }
 
       delay(10000);
       lcd.clear();
       lcd.print("Temperature "); 
-      IncomingMessage="";   //Clear String for next message
+      
+
     }
     else{
       IncomingMessage += recieved;
@@ -77,25 +116,8 @@ void setup()
   }
   
 }
-Temp=random(32,100);
-lcd.setCursor(12,0);
-lcd.print(Temp);
-lcd.print(char(223));
-	// if (tempmode==0)
-	// {
-	// 	lcd.print("F");
-	// }
-  lcd.setCursor(0,1);
-  Temp1=analogRead(TempPin);
+delay(1000);
 
-  lcd.print(Temp1);
-  Serial.println(Temp1);
-
-  delay(1000);
-  r=0;
-  b=0;
-  g=255;
-  setBacklight(r,g,b);
 }
 
 
