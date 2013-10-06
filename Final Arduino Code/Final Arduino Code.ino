20d05eb89808113c884638203cefdbf2a6161e44
RyanEggert <Ryan.Eggert@students.olin.edu>
2013-10-06 12:37:33 -0400

    More Comments & Cosmetic Changes

--- Final Arduino Code/Final Arduino Code.ino
+++ Final Arduino Code/Final Arduino Code.ino
@@ -84,7 +84,7 @@ byte box4 [8] = {
 
 
 void setup() 
-{
+  {
   pinMode (buttonPin, INPUT); 
   pinMode(tempPin,INPUT);
  	analogReference(EXTERNAL);  //Use 3.3 volt supply as AREF
@@ -93,131 +93,125 @@ void setup()
   lcd.begin(16, 2);
   lcd.clear();
 
-  	//Set up the backlight controls
-    pinMode(blRed, OUTPUT);
-    pinMode(blGrn, OUTPUT);
-    pinMode(blBlu, OUTPUT);
+	//Set up the backlight controls
+  pinMode(blRed, OUTPUT);
+  pinMode(blGrn, OUTPUT);
+  pinMode(blBlu, OUTPUT);
 
-    lcd.createChar (1, box1); //create custom characters
-    lcd.createChar (2, box2);
-    lcd.createChar (3, box3);    
-    lcd.createChar (4, box4);
+  lcd.createChar (1, box1); //create custom characters
+  lcd.createChar (2, box2);
+  lcd.createChar (3, box3);    
+  lcd.createChar (4, box4);
 
-    lcd.setCursor(0,0);       //Sets cursor at the origin
-    lcd.print("Temperature: "); //Prints "Temperature :" label to first line
+  lcd.setCursor(0,0);       //Sets cursor at the origin
+  lcd.print("Temperature: "); //Prints "Temperature :" label to first line
 
-    brightness = 255; //Sets screen to maximum brightness
-    Serial.begin(9600); //Start the serial port
-    Serial.println("1050"); //The Arduino prints to the serial port to indicate that it has loaded and is ready to recieve information. Used to alert Python code that Arduino has loaded after Pyserial forces an Arduino restart.
+  brightness = 255; //Sets screen to maximum brightness
+  Serial.begin(9600); //Start the serial port
+  Serial.println("1050"); //The Arduino prints to the serial port to indicate that it has loaded and is ready to recieve information. Used to alert Python code that Arduino has loaded after Pyserial forces an Arduino restart.
   }
 
 
-  void loop() 
+void loop() 
+{
+  long tempReading = analogRead (tempPin); //Read temperature from sensor
+  long voltage = (tempReading*3300)/1023;  //Convert to voltage (in mV)
+
+  //Debounce Button
+  int reading = digitalRead (buttonPin); 
+
+  if (reading != lastButtonState)//Looking for a change in the button's state
   {
-
-    long tempReading = analogRead (tempPin); //Read temperature from sensor
-    long voltage = (tempReading*3300)/1023;  //Convert to voltage (in mV)
-
-    //Debounce Button
-    int reading = digitalRead (buttonPin); 
-
-    if (reading != lastButtonState)//Looking for a change in the button's state
+    lastDebounceTime = millis (); //Saves the time at which the state changed
+  }
+  if ((millis () - lastDebounceTime > debounceDelay )) //Check whether its been debounceDelay milliseconds since the button's state initially changed
+  {
+    if (reading != buttonState) //Check whether the button is currently the opposite of what it began as
     {
-      lastDebounceTime = millis (); //Saves the time at which the state changed
-    }
-    if ((millis () - lastDebounceTime > debounceDelay )) //Check whether its been debounceDelay milliseconds since the button's state initially changed
-    {
-      if (reading != buttonState) //Check whether the button is currently the opposite of what it began as
+      buttonState = reading; //Store current reading
+
+      if (buttonState == HIGH) //Only switch if the button is HIGH
       {
-        buttonState = reading; //
+        i++; //increments i, used to select between C and F
+        TempRunOnce=1; //Sets TempRunOnce=0; this displays the temp once per button press to prevent screen flicker
+        lcd.setCursor(0,0); //Sets LCD cursor at the origin
+        lcd.print("Temperature: "); //Prints temperature text label
+      }
+    }
+  }
 
-      // only toggle the LED if the new button state is HIGH
-        if (buttonState == HIGH) 
-        {
-          i++; //increments i, used to select between C and F
-          TempRunOnce=1; //Sets TempRunOnce=0; this displays the temp once per button press to prevent screen flicker
-          lcd.setCursor(0,0); //Sets LCD cursor at the origin
-          lcd.print("Temperature: "); //Prints temperature text label
+  lastButtonState = reading; //Stores the latest reading from the button
+
+  if (( i % 2)== 0) //checks whether i is an even number
+  {
+    if(TempRunOnce==1) //checks whether this is the first time through this if-statement since the button has been pressed
+    {
+      tempC1 = ((voltage - 500)/10);//converting from 10 mv per degree wit 500 mV offset
+      //to degrees ((voltage - 500mV) times 100)
+      tempC2 =((voltage - 500)%10) ;                                     
+      lcd.setCursor(0,1);
+      lcd.print(tempC1);
+      lcd.print ("."); 
+      lcd.print(tempC2); 
+      lcd.print (char (223)); 
+      lcd.println ("C          ");
+      Serial.print(tempC1); Serial.print(".");Serial.print(tempC2);Serial.println("C");
+      g=map(tempC1,20,30,0,255); //map green backlight LED to display 0 when temp is 20C, 255 when temp is 
+      b=map(tempC1,20,30,255,0); //map green backlight LED to display 0 when temp is 20C, 255 when temp is  
+      r=0;
+      setBacklight(r,g,b);//Use the setBacklight function to change backlight
+      ShowCustomChar();
+      TempRunOnce++ ;
+    }
+  }
+  if( i%2 !=0) //checks whether i is an odd number
+  {
+    if (TempRunOnce==1)
+    {
+     // now convert to Fahrenheit
+     tempF1 = (((((voltage-500)/10)*9)/5)+32);
+     tempF2 = (((((voltage-500)%10)*9)%5)+32);
+     lcd.setCursor(0,1);
+     lcd.print(tempF1);
+     lcd.print ("."); 
+     lcd.print(tempF2); 
+     lcd.print (char (223)); //Print the degree sign to the LCD
+     lcd.println ("F         "); 
+     Serial.print(tempF1); Serial.print(".");Serial.print(tempF2);Serial.println("F");
+     g=map(tempC1,65,80,0,255);
+     b=map(tempC1,65,80,255,0);
+     r=0;
+     setBacklight(r,g,b);
+     ShowCustomChar();
+     TempRunOnce++;
+   }
+ }
+    if ( Serial.available() > 0) //Checks for incoming data from the serial port
+    {
+      while(Serial.available() >0) //Loops as long as there is incoming data
+      {
+       char recieved=Serial.read(); //Stores the recieved character
+       
+       if (recieved==char(003)) //Check to see whether  character has been recieved. Python appends this to the end of each custom message it sends.
+       {
+        lcd.clear(); //Clear the LCD
+        lcd.print(IncomingMessage); //Print everything recieved to the serial port, excluding the  character to the LCD
+        delay(10000); //Display the message for 10 seconds
+        lcd.clear(); //Clear the message from the screen
+        lcd.print("Press button to"); //Prompt the user to press the button to 
+        lcd.setCursor(0,1);           //resume temperature display
+        lcd.print("continue.");
+          IncomingMessage="";   //Clear IncomingMessage string for next message
+        }
+        else{
+          IncomingMessage += recieved; //if the message isn't over yet, append the latest recieved character to a string
         }
       }
     }
-
-     lastButtonState = reading; //Stores the latest reading from the button
-
-    if (( i % 2)== 0) //checks whether i is an even number
-    {
-      if(TempRunOnce==1) //checks whether this is the first time through this if-statement since the button has been pressed
-      {
-        tempC1 = ((voltage - 500)/10);//converting from 10 mv per degree wit 500 mV offset
-        //to degrees ((voltage - 500mV) times 100)
-        tempC2 =((voltage - 500)%10) ;                                     
-        lcd.setCursor(0,1);
-        lcd.print(tempC1);
-        lcd.print ("."); 
-        lcd.print(tempC2); 
-        lcd.print (char (223)); 
-        lcd.println ("C          ");
-        Serial.print(tempC1); Serial.print(".");Serial.print(tempC2);Serial.println("C");
-        g=map(tempC1,20,30,0,255); //map green backlight LED to display 0 when temp is 20C, 255 when temp is 
-        b=map(tempC1,20,30,255,0); //map green backlight LED to display 0 when temp is 20C, 255 when temp is  
-        r=0;
-        setBacklight(r,g,b);//Use the setBacklight function to change backlight
-        ShowCustomChar();
-        TempRunOnce++ ;
-      }
-    }
-    if( i%2 !=0) //checks whether i is an odd number
-    {
-      if (TempRunOnce==1)
-      {
-         // now convert to Fahrenheit
-         tempF1 = (((((voltage-500)/10)*9)/5)+32);
-         tempF2 = (((((voltage-500)%10)*9)%5)+32);
-         lcd.setCursor(0,1);
-         lcd.print(tempF1);
-         lcd.print ("."); 
-         lcd.print(tempF2); 
-         lcd.print (char (223)); //Print the degree sign to the LCD
-         lcd.println ("F         "); 
-         Serial.print(tempF1); Serial.print(".");Serial.print(tempF2);Serial.println("F");
-         g=map(tempC1,65,80,0,255);
-         b=map(tempC1,65,80,255,0);
-         r=0;
-         setBacklight(r,g,b);
-         ShowCustomChar();
-         TempRunOnce++;
-       }
-     }
-
-
-
- if ( Serial.available() > 0) //checks for incoming data from the serial port
- {
-  while(Serial.available() >0) //loops as long as there is incoming data
-  {
-   char recieved=Serial.read(); //stores the recieved character
-   
-   if (recieved==char(003))
-   {
-    lcd.clear();
-    lcd.print(IncomingMessage);
-    delay(10000);
-    lcd.clear();
-    lcd.print("Press button to"); 
-    lcd.setCursor(0,1);
-    lcd.print("continue.");
-      IncomingMessage="";   //Clear String for next message
-    }
-    else{
-      IncomingMessage += recieved;
-    }
-  }
-}
 }
 
-void ShowCustomChar()
+void ShowCustomChar() //A function which will print four custom characters in their appropriate places at the far right side of the LCD.
 {
-
   lcd.setCursor(13,0); //Set cursor to print boxes 1-3 on the first line
   lcd.write (1); //Print box 1
   lcd.write (2); //Print box 2
